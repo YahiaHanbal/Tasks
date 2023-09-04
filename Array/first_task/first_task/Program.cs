8e4a553873fdf94a296b2d0a022f622db0ee6e3c
Yahia <yahiastudy@gmail.com>
2023-09-05 00:22:22 +0300

    Initial commit

---
+++ Array/first_task/first_task/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_task
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+
+            int[] nums = { 1, 55, 51 , 878 };
+            for (int i = 0; i < nums.Length; i++) {
+                Console.WriteLine(10 * nums[i]);
+            }
+        }
+    }
+}
